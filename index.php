54a95e0f1756db7cc16038a9df3c70ddb80a4a0d
Lucien Guimier <lucien.guimier@laposte.net>
2015-02-07 15:27:23 +0100

    Documented $root/index.php
    Removed not working locale definition.

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <?php
 
+/*----- Tools -----*/
+
+/** Get the absolute path of a file in the file system.
+ * @param string $relative Relative path of the file from the project root directory.
+ */
 function path( $relative ) {
 	return __DIR__ . '/' . $relative;
 }
 
+/* Add classes paths. */
 set_include_path( implode( ':', array(
 	path( 'classes' ),
 	path( 'classes/pages' ),
 	path( 'classes/log' )
 ) ) );
 
+/** Class autoloader.
+ * @see http://php.net/manual/en/function.autoload.php
+ * @param $class Class to load.
+ */
 function __autoload( $class ) {
 	require_once "$class.php";
 }
 
+/*----- Configuration -----*/
+
 $config = json_decode( file_get_contents( __DIR__ . '/config.json' ), true );
-setlocale( LC_ALL, $config['locale'] );
-setlocale( LC_ALL, 'fr_FR' );
+
+/*----- Output -----*/
 
 $pageClass = 'IndexPage';
 
